0308294b62047567f7f38a59c3faa02ddca7d8c9
Juan David Vega <jdvr.93@hotmail.com>
2017-02-04 20:48:42 +0100

    update readme and basic example

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # mustache-static-website
 A lightweight way to generate flexible static websites. Mustache and Node.
 
-## work in progress...
+## How does it work?
+
+Use _tpl_ folder to add _mustache_ templates and then use _generable-pages_ folder to declare with pages do you want to generate and relationship between templates.
+
+This project is very simple and focus on the idea to provide just a structure to a static site project.
+ 
+After setting all run:
+
+```
+npm generate
+```
+
+or
+
+```
+npm watch
+```
+
+
+[Personal blog post](http://juandavidvega.es/2016/10/07/Generating-static-websites-with-Mustache-and-Node/)
--- generable-pages/index-model.js
+++ generable-pages/index-model.js
@@ -1,18 +1,16 @@
-var generationData = {};
-
-generationData.partials = [
-  "-p tpl/partials/head.mustache",
-  "-p tpl/partials/nav.mustache",
-  "-p tpl/partials/budget-calculator.mustache"
-];
-generationData.model = {
-    name: "views/index/es/view.json",
-    //the order is important on this partials
-    partials:[
-      ...
-    ]
+var generationData = {
+    partials: [
+        "-p tpl/partials/head.mustache",
+        "-p tpl/partials/nav.mustache",
+        "-p tpl/partials/budget-calculator.mustache"
+    ],
+    model: {
+        name: "views/index/es/view.json",
+        //the order is important on this partials
+        partials: []
+    },
+    target: "tpl/index.mustache",
+    out: "index.html"
 };
-generationData.target = "tpl/index.mustache";
-generationData.out = "index.html"
 
 module.exports = generationData;
